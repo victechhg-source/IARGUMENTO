20082740d54dd1a2108180fe84bb5e547bcbbab8
Victor Azevedo <victor.hugov007@gmail.com>
2026-07-29 18:48:41 +0000

    Visual edits

--- src/components/essay/EssayListItem.jsx
+++ src/components/essay/EssayListItem.jsx
@@ -37,7 +37,7 @@ export default function EssayListItem({ essay, onDelete }) {
         <div className="text-right">
           {essay.status === 'completed' ?
           <>
-              <p className="font-bold text-sm text-[#e9861a] bg-[#fae8db]" style={{ color: banca?.color }}>{essay.final_grade}/{maxGrade}</p>
+              <p className="font-bold text-sm bg-[#ffffff]" style={{ color: banca?.color }}>{essay.final_grade}/{maxGrade}</p>
               <p className="text-xs text-muted-foreground">{percentage}%</p>
             </> :
 
